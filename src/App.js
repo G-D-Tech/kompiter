3a8a35864669414ebd81924928b8973435e7b5f2
haakonmd <haakonmd@stud.ntnu.no>
2021-06-25 10:45:31 +0200

    Add Web Routing

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
-import './App.css';
-import { BsFillPersonFill} from "react-icons/bs";
-
+import Homepage from "../src/screens/Homepage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CreateGroup from "../src/screens/CreateGroup";
 
 function App() {
   return (
-    <div class="container">
-
-    <div class="icon"><BsFillPersonFill size={30}/></div> 
-
-    <gruppeOverskrift>Group</gruppeOverskrift>
-    <div /* class="d-flex justify-content-between" */ class="d-flex justify-content-center">
-      <form class="form-inline" >
-      <div>
-        <input class="form-control" id="exampleFormControlInput" placeholder="123456"></input>
+    <Router>
+      <div className="content">
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route path="/CreateGroup">
+            <CreateGroup />
+          </Route>
+        </Switch>
       </div>
-      <button class="RedButtonStyle">Join Group</button>
-      </form>
-    </div>
-    <div class="d-flex justify-content-center">
-    <button class="RedButtonStyle" id="createGroupButton">Create Group</button>
-    </div>
-    <div class="d-flex justify-content-center">
-    <button class="GroupButtonStyle" id="createGroupButton"><gruppeTekst>Group 1</gruppeTekst></button>
-    </div>
-    <div class="d-flex justify-content-center">
-    <button class="GroupButtonStyle" id="createGroupButton"><gruppeTekst>Group 2</gruppeTekst></button>
-    </div>
-    <div class="d-flex justify-content-center">
-    <button class="GroupButtonStyle" id="createGroupButton"><gruppeTekst>Group 3</gruppeTekst></button>
-    </div>
-
-    
-    </div>
-    
+    </Router>
   );
 }
 
---
+++ src/screens/CreateGroup.js
@@ -0,0 +1,9 @@
+const CreateGroup = () => {
+  return (
+    <div className="CreateGroup">
+      <h2>Add a new group</h2>
+    </div>
+  );
+};
+
+export default CreateGroup;
