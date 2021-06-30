0b9bb2e97811270a220ade4f3a88d5c842f88ebf
haakonmd <haakonmd@stud.ntnu.no>
2021-06-30 09:07:50 +0200

    Add Firestore and CreateGroup

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmTlUkT6YUuUz2FW5oPJ6mI05YxoLuo5E",
+  authDomain: "kompiter-907ab.firebaseapp.com",
+  projectId: "kompiter-907ab",
+  storageBucket: "kompiter-907ab.appspot.com",
+  messagingSenderId: "898063878885",
+  appId: "1:898063878885:web:e73ad5f9f3664edf4fa20c",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
--- src/screens/Homepage.js
+++ src/screens/Homepage.js
@@ -1,51 +1,74 @@
 import { BsFillPersonFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import "../styles/Homepage.css";
+import { useEffect, useState } from "react";
 
-const Homepage = () => {
+import { propTypes } from "react-bootstrap/esm/Image";
+import firebase from "../firebase";
+
+function Homepage() {
+  const [groups, setGroups] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const ref = firebase.firestore().collection("groups");
+
+  function getGroups() {
+    setLoading(true);
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setGroups(items);
+      setLoading(false);
+    });
+  }
+
+  useEffect(() => {
+    getGroups();
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <div class="container">
-      <div class="icon">
-        <BsFillPersonFill size={30} />
+    <div>
+      <div id="d-flex justify-content-center"></div>
+      <div class="container">
+        <div class="icon">
+          <BsFillPersonFill size={30} />
+        </div>
+
+        <h1 id="groupHead">Group</h1>
+        <div class="d-flex justify-content-center">
+          <form class="form-inline">
+            <div>
+              <input
+                class="form-control"
+                id="exampleFormControlInput"
+                placeholder="123456"
+              ></input>
+            </div>
+            <button class="RedButtonStyle">Join Group</button>
+          </form>
+        </div>
+        <div class="d-flex justify-content-center">
+          <button class="RedButtonStyle" id="createGroupButton">
+            <Link to="/CreateGroup">Create Group</Link>
+          </button>
+        </div>
       </div>
 
-      <h1 id="groupHead">Group</h1>
-      <div
-        /* class="d-flex justify-content-between" */ class="d-flex justify-content-center"
-      >
-        <form class="form-inline">
-          <div>
-            <input
-              class="form-control"
-              id="exampleFormControlInput"
-              placeholder="123456"
-            ></input>
-          </div>
-          <button class="RedButtonStyle">Join Group</button>
-        </form>
-      </div>
-      <div class="d-flex justify-content-center">
-        <button class="RedButtonStyle" id="createGroupButton">
-          <Link to="/CreateGroup">Create Group</Link>
-        </button>
-      </div>
-      <div class="d-flex justify-content-center">
-        <button class="GroupButtonStyle" id="createGroupButton">
-          <h2 id="groupText">Group 1</h2>
-        </button>
-      </div>
-      <div class="d-flex justify-content-center">
-        <button class="GroupButtonStyle" id="createGroupButton">
-          <h2 id="groupText">Group 2</h2>
-        </button>
-      </div>
-      <div class="d-flex justify-content-center">
-        <button class="GroupButtonStyle" id="createGroupButton">
-          <h2 id="groupText">Group 3</h2>
-        </button>
-      </div>
+      {groups.map((group) => (
+        <div key={group.id} class="d-flex justify-content-center">
+          <button id="groupButton">
+            <text id="groupText">{group.groupName}</text>
+          </button>
+        </div>
+      ))}
     </div>
   );
-};
+}
 
 export default Homepage;
