dcee5cba8af13861ebf657e30ccadf82baea009c
haakonmd <haakonmd@stud.ntnu.no>
2021-07-06 11:51:25 +0200

    Add deleteGroupButton at GroupPage

--- src/screens/GroupPageSetting.js
+++ src/screens/GroupPageSetting.js
@@ -2,8 +2,10 @@ import "../styles/Homepage.css";
 import "../styles/ModalGroup.css";
 
 import GroupPageNavBar from "../screens/GroupPageNavBar";
+import { Link } from "react-router-dom";
 
 import { useLocation } from "react-router-dom";
+import firebase from "../firebase";
 
 function GroupPageSetting() {
   const location = useLocation();
@@ -32,10 +34,36 @@ function GroupPageSetting() {
       </div>
     </div>
   ));
+
+  const ref = firebase.firestore().collection("groups");
+
+  function deleteGroup(group) {
+    ref
+      .doc(group.id)
+      .delete()
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
   return (
     <div className="modalGroup-content">
       {GroupPageNavBar(group, startDate, endDate)}
       <div>{listSettings}</div>
+      <div className="display-challenges">
+        <label className="uncompletedChallengesText">Delete this group</label>
+        {/* <label className="deleteGroupText">
+            Once you deleted a group, there is no going back
+          </label> */}
+        <Link to="/">
+          <button
+            className="DeleteButtonStyle"
+            onClick={() => deleteGroup(group)}
+          >
+            Del
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/screens/Homepage.js
+++ src/screens/Homepage.js
@@ -1,16 +1,18 @@
 import { BsFillPersonFill } from "react-icons/bs";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Homepage.css";
 import "../styles/ModalGroup.css";
 import firebase from "../firebase";
-import { IoIosClose } from "react-icons/io";
 
 const Homepage = () => {
   const [groups, setGroups] = useState([]);
 
-  const ref = firebase.firestore().collection("groups");
+  // Create Ref
+  const isMounted = useRef(false);
+  //const [error, setError] = useState("");
 
+  // Create a function for fetching your data   See: https://dev.to/olimpioadolfo/how-to-cleanup-firestore-data-fetch-on-useeffect-18ed
   const getGroups = () => {
     firebase
       .firestore()
@@ -20,25 +22,20 @@ const Homepage = () => {
         querySnapshot.forEach((doc) => {
           items.push(doc.data());
         });
-        setGroups(items);
+        if (isMounted.current) {
+          setGroups(items);
+        }
       });
+    /* .catch((error) => {
+        isMounted.current && setError(error);
+      }); */
   };
 
-  function deleteGroup(group) {
-    ref
-      .doc(group.id)
-      .delete()
-      .catch((err) => {
-        console.error(err);
-      });
-  }
-
   useEffect(() => {
+    isMounted.current = true;
     getGroups();
-    console.log("Build");
-    return () => {
-      console.log("Cleanup");
-    };
+    // this is run when component unmount
+    return () => (isMounted.current = false);
   }, []);
 
   return (
@@ -74,7 +71,7 @@ const Homepage = () => {
       </div>
 
       {groups.map((group) => (
-        <div key={group.id}>
+        <div className="wrap" key={group.id}>
           <Link
             to={{
               pathname: "/GroupPage",
@@ -86,11 +83,6 @@ const Homepage = () => {
             }}
           >
             <button className="GroupButtonStyle " id="createGroupButton">
-              <IoIosClose
-                onClick={() => deleteGroup(group)}
-                className="crossButton"
-                size={40}
-              ></IoIosClose>
               <div className="d-flex justify-content-center" id="groupText">
                 {group.groupName}
               </div>
